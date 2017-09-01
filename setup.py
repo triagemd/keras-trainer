9e6a8359b7886e43a3975367ea9c0149cfbe7a54
Steven Normore <snormore@gmail.com>
2017-08-31 20:53:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-trainer',
-    version='0.0.1',
+    version='0.0.2',
     description='A training abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
