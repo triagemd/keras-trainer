62d0f474d189b963330bbf046b39e061aa8b57c2
jsalbert <albert@gpu-albert-4x.c.triage-lab.internal>
2017-10-02 16:46:01 +0000

    variable never used

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -38,7 +38,7 @@ def run_parallel_test(data_generator):
 
     trained_epochs = []
     tracker_cb = LambdaCallback(on_epoch_begin=lambda epoch, logs: trained_epochs.append(epoch))
-    out = model.fit_generator(data_generator(4),
+    model.fit_generator(data_generator(4),
                               steps_per_epoch=3,
                               epochs=5,
                               initial_epoch=2,
