8655c27c369045aaa59b41fbb57fe22c4c31d092
Steven Normore <snormore@gmail.com>
2017-10-17 14:03:39 -0400

    Fix run.py

--- keras_trainer/run.py
+++ keras_trainer/run.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('model_spec', type=str, help='name of the base model spec')
-    parser.add_argument('dictionary', type=str, help='comma separated list of class names')
     parser.add_argument('train_dataset_dir', type=str, help='location of the training dataset directory')
     parser.add_argument('val_dataset_dir', type=str, help='location of the validation dataset directory')
     parser.add_argument('output_model_dir', type=str, help='location of the training output model directory')
@@ -20,6 +19,5 @@ if __name__ == '__main__':
         parser.add_argument('--%s' % (key, ), **kwargs)
     args = parser.parse_args()
 
-    dictionary = args.dictionary.split(',')
-    trainer = Trainer(args.model_spec, dictionary, args.train_dataset_dir, args.val_dataset_dir, args.output_model_dir, args.output_logs_dir)
+    trainer = Trainer(args.model_spec, args.train_dataset_dir, args.val_dataset_dir, args.output_model_dir, args.output_logs_dir)
     trainer.run()
