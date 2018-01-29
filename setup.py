da7d03b5ffe8ce2e3c8a4984f6888eb990ad19e3
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2018-01-29 04:44:59 -0500

    multi-gpu when num_gpus >1 instead of 0

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-trainer',
-    version='0.0.14',
+    version='0.0.15',
     description='A training abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     install_requires=[
-        'Keras',
+        'Keras>=2.0.0',
         'h5py',
         'Pillow',
         'keras-model-specs>=0.0.16',
