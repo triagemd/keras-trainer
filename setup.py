0d995ec0bb4f9f3f33614c0398e443b7d7b8e3f0
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2018-05-15 11:17:53 -0400

    update keras version

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-trainer',
-    version='0.0.20',
+    version='0.0.23',
     description='A training abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     install_requires=[
-        'Keras',
+        'Keras>=2.1.6',
         'h5py',
         'Pillow',
-        'keras-model-specs>=0.0.16',
+        'keras-model-specs>=0.0.24',
     ]
 )
