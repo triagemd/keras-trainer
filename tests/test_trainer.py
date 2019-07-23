259b80a7df2e4e07b4f31a534ecaba8a4c90dc80
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2019-07-23 16:52:12 -0400

    Update to support new version of keras-model-specs

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -223,7 +223,7 @@ def test_freeze_layers_on_catdog_datasets(train_catdog_dataset_path, val_catdog_
     check_freeze_layers_train_on_catdog_datasets_str(train_catdog_dataset_path, val_catdog_dataset_path,
                                                      freeze_layer_lst, trainer_args, expected_model_spec)
 
-    with pytest.raises(ValueError, match="<class 'numpy.float64'> layer type not supported to freeze layers, "
+    with pytest.raises(ValueError, match="<type 'numpy.float64'> layer type not supported to freeze layers, "
                                            "we expect an int giving the layer index or a str containing "
                                            "the name of the layer."):
         check_freeze_layers_train_on_catdog_datasets_with_float(train_catdog_dataset_path, val_catdog_dataset_path,
@@ -346,7 +346,7 @@ def test_resnet50_on_catdog_datasets(train_catdog_dataset_path, val_catdog_datas
     expected_model_spec = {'klass': 'keras.applications.resnet50.ResNet50',
                            'name': 'resnet50',
                            'preprocess_args': [1, 2, 3],
-                           'preprocess_func': 'mean_subtraction',
+                           'preprocess_func': 'bgr_mean_subtraction',
                            'target_size': [512, 512, 3]
                            }
     check_train_on_catdog_datasets(train_catdog_dataset_path, val_catdog_dataset_path,
