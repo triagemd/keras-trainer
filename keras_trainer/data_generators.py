535b491d15b8e2011b0d812ca154ed53a226e52c
Anirudh Challa <anirudh@triage.com>
2020-03-12 11:10:56 -0400

    Update keras_trainer/data_generators.py
    Co-Authored-By: Adria Romero <adriaromero@me.com>

--- keras_trainer/data_generators.py
+++ keras_trainer/data_generators.py
@@ -748,7 +748,7 @@ class EnhancedImageDataGenerator(ImageDataGenerator):
     - random_crop_size:  Size of the random crop. Either a percentage of the original image (0,1) that will do square
         crop, a fixed size (tuple), or integer where the value will set equally to both dimensions.
         target_size: tuple of integers, dimensions to resize input images to.
-    - custom_crop: A boolean, if True custom crops will be done according to the value in 'z_col'. This has been
+    - custom_crop: A boolean, if True custom crops will be done according to the value in `z_col`. This has been
         implemented only for  `flow_from_dataframe`.
 
     The functions `flow_from_dataframe` and `flow_from_directory` incorporate the following extra variables:
